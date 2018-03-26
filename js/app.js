fc8b334b31b3a0ba66babf0f3368e2e059f2e238
Dawid Śliwiński <sliwinskidawid12@gmail.com>
2018-03-26 11:25:39 +0200

    Remove dismatched items

--- js/app.js
+++ js/app.js
@@ -86,27 +86,29 @@ deck.innerHTML = shuffledCardsString;
  |*  - display the card's symbol (put this functionality in another function that you call from this one)
  |*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  |*  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ |*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ |*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 var card = document.querySelectorAll(".card");
 
 let cardList = [];
+let click = 0;
 
 for (let i = 0; i < card.length; i++) {
     card[i].addEventListener("click", function() {
-        
         displaySymbol();
         addCardToList();
+        click++;
+        console.log(cardList);
         
-        if(cardList.length != 0) {
-            if(cardList[cardList.length-1] == cardList[cardList.length-2]) {
+        if(click%2 == 0 && cardList.length != 0) {
+            if(cardList[cardList.length-1].innerHTML == cardList[cardList.length-2].innerHTML) {
                 lockCards();
             }
-            else {
-                removeCards();
+            else if (cardList[cardList.length-1].innerHTML != cardList[cardList.length-2].innerHTML){
+                setTimeout(removeCards, 1000);
             }
         }
         
@@ -124,7 +126,10 @@ for (let i = 0; i < card.length; i++) {
         cardList[cardList.length-2].classList.add("match");
     }
     function removeCards() {
-        
+        cardList[cardList.length-1].classList.remove("show", "open");
+        cardList[cardList.length-2].classList.remove("show", "open");
+        cardList.pop();
+        cardList.pop();
     }
 }
 
